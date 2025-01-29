6c78da7c77f6d76e8429b98d3b5ee6d39b8a6d73
Jac Lemieux <jac@smartlayer.ventures>
2025-01-29 09:58:02 -0800

    single summary per day, with voice and email

--- migrations/018_add_summarization_type_to_summary.sql
+++ migrations/018_add_summarization_type_to_summary.sql
@@ -2,4 +2,4 @@
 ALTER TABLE summary ADD COLUMN summarization_type VARCHAR(255);
 
 -- Update existing records to have 'summarize_content' as the default value
-UPDATE summary SET summarization_type = 'summarize_content' WHERE summarization_type IS NULL; 
+UPDATE summary SET summarization_type = 'summarize_content' WHERE summarization_type IS NULL; mode
